e70bd78db24b46301e0d5fae1fc2ff310f342e8a
Ivan Lo <ivan.lo@edu.sait.ca>
2020-03-02 08:25:17 -0700

    Temperature asp.net

---
+++ Temperature-Ivan/Templab1-Ivan/Temp.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Temp.aspx.cs" Inherits="Templab1_Ivan.Temp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Temperature Converter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link href="Content/site.css" rel="stylesheet" />
+    <script src="Scripts/jquery-3.0.0.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <header class="jumbotron text-center">
+            <h1>Temperature Converter:</h1>
+
+        </header>
+
+        
+        <main>
+            <form id="form1" class="form-horizontal" runat="server">
+        <div class = "form-group row">
+            <asp:Image ID="Image1" runat="server" Height="279px" ImageUrl="~/Images/PW-2015-06-10-Johnston-thermometer.jpg" Width="362px" />
+        </div>
+
+        <div class = "form-group row">
+            
+            <label class="col-sm-3">From:</label>
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem>Fahrenheit</asp:ListItem>
+                <asp:ListItem>Celsius</asp:ListItem>
+                <asp:ListItem>Kelvin</asp:ListItem>
+                </asp:DropDownList>
+            <label class="col-sm-4">Input Temperature:</label>
+            <asp:TextBox ID="txtTemperature" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" class="error" runat="server" ErrorMessage="Temperature Input Required" ControlToValidate="txtTemperature" Display="Dynamic"></asp:RequiredFieldValidator>
+            <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="Input must be between -1000 and 1000" ControlToValidate="txtTemperature" Display="Dynamic" MaximumValue="1000" MinimumValue="-1000" Type="Double"></asp:RangeValidator>
+        </div>
+
+            <div class = "form-group row">
+                
+                <label class="col-sm-3">To:</label>
+                <asp:DropDownList ID="DropDownList2" runat="server">
+                <asp:ListItem>Fahrenheit</asp:ListItem>
+                <asp:ListItem>Celsius</asp:ListItem>
+                <asp:ListItem>Kelvin</asp:ListItem>
+                </asp:DropDownList>
+                <label class="col-sm-4">Output Temperature:</label>
+               
+                <asp:Label ID="lbloutTemp" runat="server"></asp:Label>
+
+                </div>
+
+              
+                
+
+
+                <div class ="form-group row">
+                    <asp:Button ID="btnConvert" runat="server" Text="Convert" OnClick="btnConvert_Click" />
+                    <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" CausesValidation="False" />
+                </div>
+
+
+        
+        </form>
+       </main>
+        <footer>&copy;2020 - by Ivan</footer>
+    </div>
+        
+      
+</body>
+</html>
---
+++ Temperature-Ivan/Templab1-Ivan/Temp.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Templab1_Ivan
+{
+    public partial class Temp : System.Web.UI.Page
+    {
+   
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // turn off unobtrusive validation
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            // Default Conversion
+            //DropDownList1.SelectedIndex = 1;
+            //DropDownList2.SelectedIndex = 0;
+            // Focus on textbox on page load
+            txtTemperature.Focus();
+        }
+
+        protected void btnConvert_Click(object sender, EventArgs e)
+        {
+            double inputTemp, outputTemp;
+            inputTemp = double.Parse(txtTemperature.Text);
+
+            // Combobox1 = Celsius and Combobox2 = Fahrenheit
+            if (DropDownList1.SelectedIndex == 1 && DropDownList2.SelectedIndex == 0)
+            {
+                outputTemp = (inputTemp * (1.8) + 32);
+                lbloutTemp.Text = Math.Round(outputTemp, 2).ToString();
+            }
+            // Combobox1 = Celsius and Combobox2 = Kelvin
+            if (DropDownList1.SelectedIndex == 1 && DropDownList2.SelectedIndex == 2)
+            {
+                outputTemp = (inputTemp + 273.15);
+                lbloutTemp.Text = Math.Round(outputTemp, 2).ToString();
+            }
+            // Combobox1 = Fahrenheit and Combobox2 = Celsius
+            if (DropDownList1.SelectedIndex == 0 && DropDownList2.SelectedIndex == 1)
+            {
+                outputTemp = ((inputTemp - 32) / 1.8);
+                lbloutTemp.Text = Math.Round(outputTemp, 2).ToString();
+            }
+            // Combobox1 = Fahrenheit and Combobox2 = Kelvin
+            if (DropDownList1.SelectedIndex == 0 && DropDownList2.SelectedIndex == 2)
+            {
+                outputTemp = ((inputTemp + 459.67) * (5/9));
+                lbloutTemp.Text = Math.Round(outputTemp, 2).ToString();
+            }
+            // Combobox1 = Kelvin and Combobox2 = Celsius
+            if (DropDownList1.SelectedIndex == 2 && DropDownList2.SelectedIndex == 1)
+            {
+                outputTemp = (inputTemp - 273.15);
+                lbloutTemp.Text = Math.Round(outputTemp, 2).ToString();
+            }
+            // Combobox1 = Kelvin and Combobox2 = Fahrenheit
+            if (DropDownList1.SelectedIndex == 2 && DropDownList2.SelectedIndex == 0)
+            {
+                outputTemp = ((inputTemp - 273.15) * 9/5 + 32);
+                lbloutTemp.Text = Math.Round(outputTemp, 2).ToString();
+            }
+
+        }
+
+        // Clears selection and focuses on input textbox
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTemperature.Text = "";
+            lbloutTemp.Text = "";
+            txtTemperature.Focus();
+        }
+
+        
+        //ignore
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}// end namespace
